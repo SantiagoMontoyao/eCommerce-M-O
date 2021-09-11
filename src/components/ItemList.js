8784db6d22f8629602b5bd7f23247dae96380d8a
SantiMontoyao <santiagomontoyao@yahoo.com>
2021-09-11 19:11:37 -0300

    se lleno el itemlist

---
+++ src/components/ItemList.js
@@ -0,0 +1,30 @@
+function Item({ item }) {
+	{id:1, 
+        nombre; "Living Things", 
+        autor; "Linkin Park", 
+        precio; 2500,
+        portada; 'linkin_living_things.jpg'}
+    
+        {id:2, 
+        nombre; "Doom Days", 
+        autor; "Bastille", 
+        precio; 1500,
+        portada;'bastille_doom_days.png'}
+    
+        {id:3, 
+        nombre; "I Trust", 
+        autor; "(G)I-DLE", 
+        precio; 1400,
+        portada;'gidle_i_trust.jpg'}
+    
+        {id:4, 
+        nombre; "Rivival", 
+        autor; "Eminem", 
+        precio; 1200,
+        portada; 'eminem_revival.jpg'}
+    
+        {id:5, 
+        nombre; "Evolve", 
+        autor; "Imagine Dragons", 
+        precio; 1650,
+        portada; 'imagine_evolve.jpg'}}
