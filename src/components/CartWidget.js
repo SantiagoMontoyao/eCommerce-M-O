5e283dce9fe3a4b06a67e4199355a1d034f34e41
SantiMontoyao <santiagomontoyao@yahoo.com>
2021-10-02 15:54:38 -0300

    se expande cart

---
+++ src/components/CartWidget.js
@@ -0,0 +1,9 @@
+const cartP = (props) => {
+    const [cart, setProductsInCart] = useState(0)
+   
+    return (
+     <CartContext.Provider value={[cart, setProductsInCart]}>
+      {props.children}
+     </CartContext.Provider>
+    )
+   }
