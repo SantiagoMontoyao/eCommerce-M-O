8224cad6dd6ffc498a102aaa0f67395d66e633d5
SantiMontoyao <santiagomontoyao@yahoo.com>
2021-10-22 17:12:25 -0300

    se modificaron los archivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+    <div class="container">
+        <div class="row">
+            <main id="items" class="col-sm-8 row"></main>
+            <aside class="col-sm-4">
+                <h2>Carrito</h2>
+                <ul id="carrito" class="list-group"></ul>
+                <p class="text-right">Total: <span id="total"></span>&dollar;</p>
+                <button id="boton-vaciar" class="btn btn-danger">Vaciar</button>
+                <button id="boton-comprar" class="btn btn-danger">Comprar</button>
+            </aside>
+        </div>
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,81 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const baseDeDatos = [
+  {
+      id: 1,
+      nombre: 'Living Things',
+      precio: 2500,
+      imagen: 'linkin_living_things.jpg'
+  },
+  {
+      id: 2,
+      nombre: 'Doom Days',
+      precio: 1500,
+      imagen: 'bastille_doom_days.png'
+  },
+  {
+      id: 3,
+      nombre: 'I Trust',
+      precio: 1400,
+      imagen: 'gidle_i_trust.jpg'
+  },
+  {
+      id: 4,
+      nombre: 'Rivival',
+      precio: 1200,
+      imagen: 'eminem_revival.jpg'
+  },
+  {
+      id: 5,
+      nombre: 'Evolve',
+      precio: 1650,
+      imagen: 'imagine_evolve.jpg'
+  },
+  {
+      id: 6,
+      nombre: 'Tim',
+      precio: 1350,
+      imagen: 'avicii_tim.png'
+  }];
+
+  function renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+        // Estructura
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        // Body
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // Titulo
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+        // Imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = info.precio + '$';
+        // Boton 
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = 'Agregar al Carrito';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+        // Insertamos
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+    });
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
